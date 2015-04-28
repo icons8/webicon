0e62494fa57922e317b3e4d6d0047833d37353b5
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-28 12:54:02 +0300

    Add image icons demo

---
+++ generator/pages/codes/angular/single-icon-image.js
@@ -0,0 +1,7 @@
+angular.module('app')
+  .config(function($i8IconProvider) {
+    $i8IconProvider
+      .icon('clock', 'assets/icons/png/clock.png')
+      .icon('calendar', 'assets/icons/png/calendar.png')
+    ;
+  });
---
+++ generator/pages/codes/jquery/single-icon-image.js
@@ -0,0 +1,8 @@
+$(function() {
+  $(document).i8icons({
+    icons: {
+      clock: 'assets/icons/png/clock.png',
+      calendar: 'assets/icons/png/calendar.png'
+    }
+  });
+});
