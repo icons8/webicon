88bcd56ebf79425ba92f1ab98930dd0488928e9b
betula <betula.icons8@gmail.com>
2015-06-22 19:03:55 +0300

    Update extensions test spec

--- test/jquery/extensions/welovesvg/extension.spec.js
+++ test/jquery/extensions/welovesvg/extension.spec.js
@@ -25,7 +25,7 @@ describe('jquery extensions welovesvg', function() {
   describe('Material Design Icons', function() {
     beforeEach(function() {
       jasmine.Ajax.stubRequest(
-        window.location.protocol + '//cdn.rawgit.com/icons8/welovesvg/b0ef1e007b125c7b02ccaf0e054bd91b19392282/libs/material-design-icons/1.0.2/material-design-icons-24px.svg'
+        window.location.protocol + '//cdn.rawgit.com/icons8/welovesvg/c10018bec3184a8b72a3d6485904c1eba4dc876f/libs/material-design-icons/1.0.2/material-design-icons-24px.svg'
       ).andReturn({
           responseText: '<svg><svg viewBox="0 0 24 24" id="perm-camera-mic"><g icon-name="perm-camera-mic-icon"></g></svg></svg>'
         });
